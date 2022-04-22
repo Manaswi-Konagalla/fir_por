845de260acd0c33b5de9afc6c9dcb7672f50ae89
manaswi <kvlmanaswi98@gmail.com>
2022-04-22 18:57:15 +0530

    first commit

---
+++ src/main/java/com/ABCCOMPANY/App.java
@@ -0,0 +1,13 @@
+package com.ABCCOMPANY;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
